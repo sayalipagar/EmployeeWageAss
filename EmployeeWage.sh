2426fda704a817849f740e7b73caaf9643ac0963
sayalipagar <pagarsayali565@gmail.com>
2022-08-01 09:47:55 +0530

    welcome UC1

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,3 +1,15 @@
 #! bin/bash
 
 echo "Welcome to Employee Wage program "
+
+isPresent=1
+
+randomCheck=$((RANDOM%2))
+
+if [ $isPresent -eq $randomCheck ]
+then
+echo "employee is present"
+else
+echo "employee is absent"
+
+fi
